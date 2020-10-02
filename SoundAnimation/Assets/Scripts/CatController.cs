db561541ede20c285323f26854df0ac85a80664e
unknown <ncmoyer99@gmail.com>
2020-10-02 17:20:15 -0400

    Animation commit

--- SoundAnimation/Assets/Scripts/CatController.cs
+++ SoundAnimation/Assets/Scripts/CatController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CatController : MonoBehaviour
 {
 public AudioClip musicClipOne;
 
@@ -10,42 +10,81 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
-// Update is called once per frame
-void Update()
+Animator anim;
+
+void Start()
+
 {
+
+  anim = GetComponent<Animator>();
+
+}
+
+// Update is called once per frame
+
+void Update()
+
+{
+
      if (Input.GetKeyDown(KeyCode.W))
+
         {
+
           musicSource.clip = musicClipOne;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.R))
+
         {
+
           musicSource.clip = musicClipTwo;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
          }
 
      if (Input.GetKeyUp(KeyCode.R))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.L))
+
         {
+
           musicSource.loop = true;
+
          }
 
      if (Input.GetKeyUp(KeyCode.L))
+
         {
+
           musicSource.loop = false;
+
         }
-   }
+
+    }
 }
